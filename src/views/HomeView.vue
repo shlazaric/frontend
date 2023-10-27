5def97ed1eedafb0d90337c5e9eed9ce737830f9
Shana Antonia <95529289+shlazaric@users.noreply.github.com>
2023-10-27 16:38:46 +0200

    Add files via upload

---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <div class="container text-center">
+      <h1> Dobrodošli na našu aplikaciju!</h1>
+      <div class="d-flex justify-content-center">
+       
+      </div>
+
+    
+      <router-link to="/popis-slastica" class="btn btn-primary mt-4">Idi na popis slastica</router-link>
+
+     
+      <div class="mt-2">
+        <router-link to="/login" class="btn btn-success mr-2"> Prijavi se </router-link>
+        <br> 
+        <router-link to="/signup" class="btn btn-info"> Registriraj se </router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+
---
+++ src/views/LoginView.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="about">
+    <h1> Prijavi se </h1>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm"></div>
+        <div class="col-sm">
+          <form>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Email adresa </label>
+              <input
+                type="email"
+                class="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                placeholder="Enter email"
+              />
+             
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1"> Lozinka </label>
+              <input
+                type="password"
+                class="form-control"
+                id="exampleInputPassword1"
+                placeholder="Password"
+              />
+            </div>
+            <div class="form-group">
+              <button type="submit" class="btn btn-primary"> Potvrdi </button>
+            </div>
+            <div class="form-group">
+              <button type="button" class="btn btn-secondary"> Otkaži </button>
+            </div>
+            <div class="form-group">
+              <button type="button" class="btn btn-link"> Zaboravio/la lozinku ?</button>
+            </div>
+          </form>
+        </div>
+        <div class="col-sm"></div>
+      </div>
+    </div>
+  </div>
+</template>
