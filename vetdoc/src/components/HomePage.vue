792f7507c7aed277f22d2e0a068717259abb0128
Shana Antonia <95529289+shlazaric@users.noreply.github.com>
2025-04-28 12:49:12 +0200

    Create HomePage

---
+++ vetdoc/src/components/HomePage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <h1>Dobrodošao na početnu stranicu!</h1>
+      <router-link to="/pet-profile">
+        <button>Dodaj ljubimca</button>
+      </router-link>
+      <router-link to="/appointment-booking">
+        <button>Rezerviraj termin</button>
+      </router-link>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HomePage'
+  }
+  </script>
+  
