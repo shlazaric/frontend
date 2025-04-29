8c75f03cc75944e32668297bcd53a07b5436af3f
Shana Antonia <95529289+shlazaric@users.noreply.github.com>
2025-04-29 17:29:48 +0200

    Dodana slika

--- vetdoc/src/components/WelcomePage.vue
+++ vetdoc/src/components/WelcomePage.vue
@@ -1,55 +1,57 @@
 <template>
-    <div class="welcome-page">
-  
+  <div class="welcome-page">
     
-  
-      <h1>Dobrodošli u VetDoc! :)</h1>
-    
-      <!-- Linkovi za navigaciju -->
-      <div class="buttons">
-        <RouterLink to="/login">
-          <button>Prijava korisnika</button>
-        </RouterLink>
-    
-        <RouterLink to="/admin-login">
-          <button>Prijava administratora</button>
-        </RouterLink>
-    
-        <RouterLink to="/register">
-          <button>Registracija</button>
-        </RouterLink>
-      </div>
+    <img src="@/assets/vet.png" alt="VetDoc Logo" class="logo" />
+
+    <h1>Dobrodošli u VetDoc! :)</h1>
+
+    <!-- Linkovi za navigaciju -->
+    <div class="buttons">
+      <RouterLink to="/login">
+        <button>Prijava korisnika</button>
+      </RouterLink>
+
+      <RouterLink to="/admin-login">
+        <button>Prijava administratora</button>
+      </RouterLink>
+
+      <RouterLink to="/register">
+        <button>Registracija</button>
+      </RouterLink>
     </div>
-  </template>
-  
-  <script setup>
-  // Nema posebne logike, ovo je samo "landing page" s linkovima
-  </script>
-  
-  <style scoped>
-  /* Stilizacija Welcome stranice */
-  .welcome-page {
-    text-align: center;
-    margin-top: 50px;
-  }
-  
- 
-  /* Stilizacija gumba */
-  .buttons {
-    margin-top: 20px;
-  }
-  
-  button {
-    margin: 10px;
-    padding: 10px 20px;
-    background-color: #4caf50;
-    color: white;
-    border: none;
-    cursor: pointer;
-  }
-  
-  button:hover {
-    background-color: #45a049;
-  }
-  </style>
-  
+  </div>
+</template>
+
+
+
+<style scoped>
+.welcome-page {
+  text-align: center;
+  margin-top: 50px;
+}
+
+.logo {
+  width: 150px;
+  height: auto;
+  display: block;
+  margin: 0 auto 20px auto;
+}
+
+
+.buttons {
+  margin-top: 20px;
+}
+
+button {
+  margin: 10px;
+  padding: 10px 20px;
+  background-color: #4caf50;
+  color: white;
+  border: none;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #45a049;
+}
+</style>
