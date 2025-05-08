fc11e059b03723dc507f464f12264a576de9fe66
Shana Antonia <95529289+shlazaric@users.noreply.github.com>
2025-05-08 19:35:48 +0200

    Update AppointmentBooking

--- vetdoc/src/components/User/AppointmentBooking.vue
+++ vetdoc/src/components/User/AppointmentBooking.vue
@@ -26,6 +26,8 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   data() {
     return {
@@ -38,27 +40,36 @@ export default {
     };
   },
   methods: {
-    submitAppointment() {
+    async submitAppointment() {
       if (!this.date || !this.time) {
         this.successMessage = '';
         this.errorMessage = 'Unesite datum i vrijeme!';
-        this.formattedDate = '';
-        this.formattedTime = '';
-      } else {
-        this.errorMessage = '';
-        this.successMessage = 'Termin je uspješno rezerviran!';
+        return;
+      }
 
-        const dateObj = new Date(this.date);
-        this.formattedDate = new Intl.DateTimeFormat('hr-HR', {
-          day: 'numeric',
-          month: 'long',
-          year: 'numeric'
-        }).format(dateObj);
+      try {
+        const response = await axios.post('http://localhost:3000/api/appointments', {
+          date: this.date,
+          time: this.time
+        });
 
-        this.formattedTime = this.time;
+        if (response.status === 201 || response.status === 200) {
+          this.errorMessage = '';
+          this.successMessage = 'Termin je uspješno rezerviran!';
+          this.formattedDate = new Intl.DateTimeFormat('hr-HR', {
+            day: 'numeric',
+            month: 'long',
+            year: 'numeric'
+          }).format(new Date(this.date));
+          this.formattedTime = this.time;
 
-        this.date = '';
-        this.time = '';
+          this.date = '';
+          this.time = '';
+        }
+      } catch (error) {
+        console.error(error);
+        this.successMessage = '';
+        this.errorMessage = 'Došlo je do greške prilikom rezervacije.';
       }
     }
   }
