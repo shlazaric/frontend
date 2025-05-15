9ff720563ddc20cca3333af62ade9233f21517eb
Shana Antonia <95529289+shlazaric@users.noreply.github.com>
2025-05-15 16:19:59 +0200

    Dodana stranica za prijavu korisnika

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Welcome from '@/views/WelcomePage.vue';
 import RegisterForm from '@/views/RegisterForm.vue';
+import Login from '@/views/UserLoginForm.vue';
 
 const routes = [
 
@@ -13,6 +14,11 @@ const routes = [
     path: '/register',
     name: 'Register',
     component: RegisterForm,
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login,
   }
 ]
 
---
+++ src/views/UserLoginForm.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="form-container">
+    <h2>Prijava korisnika</h2>
+    <form @submit.prevent="handleSubmit">
+      <label>Unesi e-mail:</label>
+      <input type="email" v-model="email" required />
+
+      <label>Unesi lozinku:</label>
+      <input type="password" v-model="password" required />
+
+      <button type="submit">Prijavi se</button>
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const email = ref('');
+const password = ref('');
+
+function handleSubmit() {
+  
+  if (!email.value || !password.value) {
+    alert('Molimo popunite oba polja!');
+    return;
+  }
+  alert(`Prijava uspješna za ${email.value}`);
+}
+</script>
+
+<style scoped>
+.form-container {
+  padding: 20px;
+}
+label {
+  display: block;
+  margin-top: 10px;
+  color: #1d006b;
+}
+input {
+  width: 100%;
+  padding: 5px;
+  box-sizing: border-box;
+}
+button {
+  margin-top: 15px;
+  background-color: #3498db;
+  color: white;
+  padding: 8px 12px;
+  border: none;
+  cursor: pointer;
+}
+button:hover {
+  background-color: #2980b9;
+}
+</style>
